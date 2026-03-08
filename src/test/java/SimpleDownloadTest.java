e0cf1b55876e51e24b2df1983f38d9172fba47ec
martin <martingeorgiev2307@gmail.com>
2026-03-08 21:21:55 +0200

    Added more tests

--- src/test/java/SimpleDownloadTest.java
+++ src/test/java/SimpleDownloadTest.java
@@ -1,12 +1,23 @@
 import org.omega.service.DownloadService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.omega.util.Method;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SimpleDownloadTest {
 
     @Test
     public void testDownload() {
-        DownloadService.download("https://www.youtube.com/watch?v=3N6YbsRxTf8");
+        DownloadService.downloadSingle("https://www.youtube.com/watch?v=3N6YbsRxTf8", Method.AUDIO);
+    }
+
+    @Test
+    public void testDownload2() {
+        DownloadService.downloadSingle("https://www.youtube.com/watch?v=3N6YbsRxTf8", Method.VIDEO);
+    }
+
+    @Test
+    public void testDownload3() {
+        DownloadService.downloadMultiple("https://youtube.com/playlist?list=OLAK5uy_kdpxq-hy6u56F2eqtN3O-q0b2eSOXs9xQ&si=eP_4I8PizJDFJMK5", Method.AUDIO);
     }
 }
